8344748d4888c6f7a1f9c31958d84d827e83174f
qing2 <qing2@mail.usf.edu>
2018-10-01 21:50:57 -0400

    add method

--- Assignment_2/StockList_Students_3.cs
+++ Assignment_2/StockList_Students_3.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using System.Linq;
 
 namespace Assignment_2
 {
@@ -10,14 +12,9 @@ namespace Assignment_2
         //summary      : Calculate the value of each node by multiplying holdings with price, and returns the total value
         //return       : total value
         //return type  : decimal
-        public decimal Value()
-        {
+        public decimal Value() {
             decimal value = 0.0m;
-            StockNode stockNode = this.head;
-            for (; stockNode != null; stockNode = stockNode.Next)
-            {
-                value += stockNode.StockHolding.Holdings * stockNode.StockHolding.CurrentPrice;
-            }
+          
             return value;
 
         }
@@ -27,40 +24,19 @@ namespace Assignment_2
         //summary      : finds the similar number of nodes between two lists
         //return       : similarty index
         //return type  : int
-        public int Similarity(StockList listToCompare)
-        {
+        public int Similarity(StockList listToCompare) {
             int similarityIndex = 0;
-            StockNode list2Node = listToCompare.head;
-            for (; list2Node != null; list2Node = list2Node.Next)
-            {
-                StockNode list1Node = this.head;
-                for (; list1Node != null; list1Node = list1Node.Next)
-                {
-                    if (list1Node.StockHolding.Name == list2Node.StockHolding.Name)
-                    {
-                        similarityIndex += 1;
-                        break;
-                    }
-                    else
-                    {
-                        continue;
-                    }
-                }
-            }
+            
             return similarityIndex;
-        }
-
-        //param        : NA
-        //summary      : Print all the nodes present in the list
-        //return       : NA
-        //return type  : NA
-        public void Print()
-        {
-            StockNode sn = this.head;
-            for (; sn != null; sn = sn.Next)
-            {
-                Console.WriteLine(sn.StockHolding.ToString());
-            }
-        }
     }
+
+    //param        : NA
+    //summary      : Print all the nodes present in the list
+    //return       : NA
+    //return type  : NA
+    public void Print()
+    {
+          
+    }
+  }
 }
